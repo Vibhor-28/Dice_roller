e47ad5612a2ddebd555ec309bc3e52d38300b977
Vibhor-28 <vibhoragarwal2802@gmail.com>
2023-09-19 14:51:29 +0530

    initial commit

---
+++ lib/diceroller.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+final randomizer = Random();
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var diceno = 1;
+  void rolldice() {
+    setState(() {
+      diceno = randomizer.nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/dice-images/dice-$diceno.png',
+          width: 200,
+        ),
+        // const SizedBox(
+        //   height: 20,
+        // ),
+        TextButton(
+          onPressed: rolldice,
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.only(top: 20),
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 28),
+          ),
+          child: const Text("Hello world"),
+        ),
+      ],
+    );
+  }
+}
